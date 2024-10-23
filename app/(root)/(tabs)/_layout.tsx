afbd2188efce7ce08d3512b59f14e2e8b2da0e1b
programmerkenzi <a23g32@gmail.com>
2024-10-23 17:17:19 +0800

    refactor: code clean up and add page structure

---
+++ app/(root)/(tabs)/_layout.tsx
@@ -0,0 +1,15 @@
+import { Stack } from "expo-router";
+
+const Layout = () => {
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="(root)" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+
+      <Stack.Screen name="+not-found" />
+    </Stack>
+  );
+};
+
+export default Layout;
